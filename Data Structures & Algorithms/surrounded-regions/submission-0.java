c84e25b2d4ad7df4d8214085bc68d86da013cb6f
mimibar <mimibar@users.noreply.github.com>
2026-09-23 13:13:30 -0600

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.java
@@ -0,0 +1,41 @@
+class Solution {
+  int m = 0;
+  int n = 0;
+  void notCapture(char[][] board, int i, int j) {
+    if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O')
+      return;
+    board[i][j] = '-';
+    notCapture(board, i - 1, j);
+    notCapture(board, i + 1, j);
+    notCapture(board, i, j - 1);
+    notCapture(board, i, j + 1);
+  }
+
+  public void solve(char[][] board) {
+    m = board.length;
+    n = board[0].length;
+
+    for (int i = 0; i < m; i++) {
+      notCapture(board, i, 0);
+      notCapture(board, i, n - 1);
+    }
+    for (int j = 0; j < n; j++) {
+      notCapture(board, 0, j);
+      notCapture(board, m - 1, j);
+    }
+    // capture
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == 'O')
+          board[i][j] = 'X';
+      }
+    }
+    // restore
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (board[i][j] == '-')
+          board[i][j] = 'O';
+      }
+    }
+  }
+}
